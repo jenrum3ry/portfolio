25350adf988cd09a63b708bb8438f7089541f852
jenrum3ry <jenrumery83@gmail.com>
2025-12-20 19:12:51 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Add this base path for GitHub Pages
-  base: '/career-spotlight-builder/',
+  base: '/portfolio/',
   
   server: {
     host: "::",
